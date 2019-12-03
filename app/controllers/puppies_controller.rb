683ddbb367c7f7166e5cd63879f9215433659a42
alexvancap <alexvancap@gmail.com>
2019-12-03 15:02:42 -0600

    creating form

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,2 +1,37 @@
 class PuppiesController < ApplicationController
+    def index
+
+    end
+
+    def register
+        @new_puppy = Puppy.new
+    end
+
+    def create
+        if !Puppy.find_by({email: params[:puppy][:email]})
+            if params[:password] == params[:repeated_password]
+                Puppy.create(allowed_params[:puppy])
+            else
+                puts "Your passwords did not match!"
+            end
+        else
+            puts "That email already exists!"
+        end
+    end
+
+    def login
+
+    end
+
+    def allowed_params
+        params.permit({
+            puppy:[
+                :name,
+                :breed,
+                :age,
+                :email,
+                :password,
+            ]
+        })
+    end
 end
--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -2,4 +2,6 @@ class Puppy < ApplicationRecord
     has_many :friendships
     has_many :posts
     has_many :friends, through: :friendships
+
+    has_secure_password
 end
